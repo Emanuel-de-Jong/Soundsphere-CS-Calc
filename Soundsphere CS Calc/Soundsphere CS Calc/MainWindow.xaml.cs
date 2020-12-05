785c2a2136a1bd1789f652bd39b12eba38b075af
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2020-12-05 13:23:52 +0100

    Done!

--- Soundsphere CS Calc/Soundsphere CS Calc/MainWindow.xaml.cs
+++ Soundsphere CS Calc/Soundsphere CS Calc/MainWindow.xaml.cs
@@ -31,32 +31,15 @@ namespace Soundsphere_CS_Calc
 
             foreach (TextBox tb in numberTextBoxes)
             {
-                DataObject.AddPastingHandler(tb, TextBoxPasteValidateNumber);
-                tb.PreviewTextInput += TextBoxValidateNumber;
-                tb.TextChanged += TextBoxTextChanged;
-            }
-        }
-
-        private void TextBoxValidateNumber(object sender, TextCompositionEventArgs e)
-        {
-            Regex regex = new Regex("[^0-9.-]+");
-            e.Handled = regex.IsMatch(e.Text);
-        }
-
-        private void TextBoxTextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextBox tb = (TextBox)sender;
-            string text = tb.Text;
-            if (text == "")
-            {
-                tb.Text = "0";
-                return;
+                DataObject.AddPastingHandler(tb, TextBox_PasteValidate);
+                tb.PreviewTextInput += TextBox_Validate;
+                tb.TextChanged += TextBox_TextChanged;
             }
 
-            Calculate();
+            CSBind.SelectionChanged += ComboBox_SelectionChanged;
         }
 
-        private void TextBoxPasteValidateNumber(object sender, DataObjectPastingEventArgs e)
+        private void TextBox_PasteValidate(object sender, DataObjectPastingEventArgs e)
         {
             bool isText = e.SourceDataObject.GetDataPresent(DataFormats.UnicodeText, true);
             if (!isText)
@@ -66,7 +49,7 @@ namespace Soundsphere_CS_Calc
             }
 
             string text = e.SourceDataObject.GetData(DataFormats.UnicodeText) as string;
-            Regex regex = new Regex("^[0-9.-]*$");
+            Regex regex = new Regex("^-?([0-9][0-9]*)(\\.[0-9]+)?$");
             if (!regex.IsMatch(text))
             {
                 e.CancelCommand();
@@ -74,10 +57,49 @@ namespace Soundsphere_CS_Calc
             }
         }
 
+        private void TextBox_Validate(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9.-]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!validate())
+            {
+                return;
+            }
+
+            Calculate();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!validate())
+            {
+                return;
+            }
+
+            Calculate();
+        }
+
+        private bool validate()
+        {
+            Regex regex = new Regex("^-?([0-9][0-9]*)(\\.[0-9]+)?$");
+
+            foreach (TextBox tb in numberTextBoxes)
+            {
+                if (tb.Text == "" || !regex.IsMatch(tb.Text))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         private void Calculate()
         {
-            float resX, resY, resW, resH;
-
             float winW = float.Parse(WinW.Text);
             float winH = float.Parse(WinH.Text);
             float csX = float.Parse(CSX.Text);
@@ -88,21 +110,30 @@ namespace Soundsphere_CS_Calc
             float objX2 = float.Parse(ObjX2.Text);
             float objY1 = float.Parse(ObjY1.Text);
             float objY2 = float.Parse(ObjY2.Text);
+            string csBind = CSBind.SelectedValue.ToString();
 
+            float baseV;
+            switch (csBind)
+            {
+                case "w":
+                    baseV = 1 / winW;
+                    break;
+                case "h":
+                    baseV = 1 / winH;
+                    break;
+                default:
+                    baseV = 1 / 100;
+                    break;
+            }
 
-            float temp1 = winW * csX;
-            resX = (1 / winH) * (temp1 != 0 ? temp1 - objX1 : objX1);
+            float winWCS = winW * csX;
+            float winHCS = winH * csY;
 
-            float temp2 = winH * csY;
-            resY = (1 / winW) * (temp2 != 0 ? temp2 - objY1 : objY1);
-
-
-            float temp3 = (1 / winH) * (temp1 != 0 ? temp1 - objX2 : objX2);
-            resW = temp3 - resX;
-
-            float temp4 = (1 / winW) * (temp2 != 0 ? temp2 - objY2 : objY2);
-            resH = temp4 - resY;
-
+            float resX, resY, resW, resH;
+            resX = baseV * (objX1 - winWCS);
+            resY = baseV * (objY1 - winHCS);
+            resW = (baseV * (objX2 - winWCS)) - resX;
+            resH = (baseV * (objY2 - winHCS)) - resY;
 
             string format = "0.0000";
             ResX.Text = resX.ToString(format);
